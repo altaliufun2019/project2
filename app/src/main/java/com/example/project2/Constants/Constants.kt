10ec56f149415e6cbf5b78c2cfa860656c426052
sajadrezvani <Rezvani@ce.sharif.edu>
2019-04-26 21:45:26 +0430

    added notif center

---
+++ app/src/main/java/com/example/project2/Constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.project2.Constants
+
+import android.content.Context
+import java.text.SimpleDateFormat
+
+object Constants {
+
+    object Tasks {
+        const val FETCH_DATA = 1
+//        const val S = 2
+    }
+}
--- app/src/main/java/com/example/project2/MessageCenter/NotificationCenter.kt
+++ app/src/main/java/com/example/project2/MessageCenter/NotificationCenter.kt
@@ -1,7 +1,61 @@
 package com.example.project2.MessageCenter
 
-object NotificationCenter {
-    interface NotificationTarget{
-        fun notified(taskId: Int)
+import android.util.SparseArray
+import com.example.project2.Constants.Constants
+import java.util.ArrayList
+
+
+class NotificationCenter private constructor() {
+    private val observers = SparseArray<MutableList<Any>>()
+    private val allowedTasks = mutableListOf<Int>()
+
+    init {
+        allowedTasks.add(Constants.Tasks.FETCH_DATA)
+    }
+
+    interface NotificationTarget {
+        fun notified(taskID: Int)
+    }
+
+    fun addTask(taskID: Int): Boolean? {
+        allowedTasks.add(taskID)
+        println("success")
+        return false
+    }
+
+    fun data_loaded() {
+        val taskID = NotificationCenter.DATA_LOADING
+        for (observer in observers.get(taskID)) {
+            (observer as NotificationTarget).notified(taskID)
+        }
+    }
+
+    fun register(observer: Any, taskID: Int): Boolean? {
+        if (!allowedTasks.contains(taskID))
+            return false
+        var taskObservers: MutableList<Any>? = observers.get(taskID)
+        if (taskObservers == null) {
+            taskObservers = mutableListOf<Any>()
+            observers.put(taskID, taskObservers)
+        }
+        if (taskObservers.contains(observer))
+            return false
+        taskObservers.add(observer)
+        return true
+    }
+
+    fun unregister(observer: Any, taskID: Int): Boolean? {
+        if (!allowedTasks.contains(taskID))
+            return false
+        val taskObservers = observers.get(taskID) ?: return false
+        if (!taskObservers.contains(observer))
+            return false
+        taskObservers.remove(observer)
+        return true
+    }
+
+    companion object {
+        val instance = NotificationCenter()
+        val DATA_LOADING = Constants.Tasks.FETCH_DATA
     }
 }
