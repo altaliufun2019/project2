503b5959a35e657e388d51daa96b3b02eadb3469
aafsari <amirafsari99@yahoo.com>
2019-04-26 19:05:39 +0430

    conflict fixed in dependencies.

--- app/src/main/java/com/example/project2/UIClasses/fragments/ActionPane.kt
+++ app/src/main/java/com/example/project2/UIClasses/fragments/ActionPane.kt
@@ -1,15 +1,11 @@
-package com.example.test.fragments
-
+package com.example.project2.UIClasses.fragments
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.Spinner
 import com.example.project2.R
 
--- app/src/main/java/com/example/project2/UIClasses/fragments/GridFragment.kt
+++ app/src/main/java/com/example/project2/UIClasses/fragments/GridFragment.kt
@@ -1,4 +1,4 @@
-package com.example.test.fragments
+package com.example.project2.UIClasses.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
